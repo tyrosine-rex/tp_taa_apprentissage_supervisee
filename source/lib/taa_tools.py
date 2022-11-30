b70bb135dba0c7446c7ef13f8bdb0518617fab86
tyrosine-rex <thomas.lahaie@etu.univ-lyon1.fr>
2022-11-30 11:25:33 +0100

    strip line

--- source/lib/taa_tools.py
+++ source/lib/taa_tools.py
@@ -1,5 +1,5 @@
 from hashlib import sha1
-from pickle import dump, load 
+from pickle import dump, load
 from sys import stderr
 from os.path import exists
 from os import makedirs
@@ -31,21 +31,21 @@ import numpy as np
 def cache_pickle(func):
     def wrapper(*args, **kwargs):
         cache_dir = f"./__cache_pickle__/{func.__name__}"
-        
+
         if not exists(cache_dir):
             makedirs(cache_dir)
-            
+
         tohash = f"{args}{kwargs}"
         basename = sha1(tohash.encode('utf-8')).hexdigest()
         pkl_path = f"{cache_dir}/{basename}.pkl"
 
-        if exists(pkl_path): 
+        if exists(pkl_path):
             res = load(open(pkl_path, "rb"))
-            print(f"Loaded from {pkl_path}", file=stderr)
+            print(f"{func.__name__}: Loaded from {pkl_path}", file=stderr)
         else:
             res = func(*args, **kwargs)
             dump(res, open(pkl_path, "wb"))
-            print(f"Saved to {pkl_path}", file=stderr)            
+            print(f"{func.__name__}: Saved to {pkl_path}", file=stderr)
         return res
     return wrapper
 
@@ -54,7 +54,7 @@ def cache_pickle(func):
 ## SKLEARN STUFF
 ##
 
-CLASSIFIERS = { 
+CLASSIFIERS = {
     'RF_10': RandomForestClassifier(n_estimators=10, random_state=1),
     'RF_20': RandomForestClassifier(n_estimators=20, random_state=1),
     'RF_40': RandomForestClassifier(n_estimators=40, random_state=1),
@@ -104,11 +104,11 @@ CLASSIFIERS = {
 def test_model(X_train, Y_train, X_test, Y_test, sk_fun, comment="", **kwargs):
 
     COLS_EVAL = ["method", "comment", "precision", "accuracy", "recall", "VP", "VN", "FP", "FN", "args"]
-    
+
     model = sk_fun(**kwargs)            # call a model
     model.fit(X_train, Y_train)         # training
     Y_pred = model.predict(X_test)      # testing
-    
+
     # metrics
     pr, acc, rec = precision_score(Y_test, Y_pred), accuracy_score(Y_test, Y_pred), recall_score(Y_test, Y_pred)
     Conf = confusion_matrix(Y_test, Y_pred)
@@ -123,7 +123,7 @@ def test_model(X_train, Y_train, X_test, Y_test, sk_fun, comment="", **kwargs):
     print(f"{acc:>16.3f}{pr:>16.3f}{rec:>16.3f}")
     print("Confusion matrix:")
     display(pd.DataFrame(Conf, index=["is 0", "is 1"], columns=["predicted 0", "predicted 1"]))
-    
+
     return line_df
 
 
@@ -147,7 +147,7 @@ def test_pipeline(pipelines, X_test, Y_test, score_params={}):
 @cache_pickle
 def run_classifiers(sk_func_dict, X, Y, scores=['accuracy', 'precision', 'roc_auc'], nb_split=10):
     COLS_EVAL_3 = [
-        'method', 'test_precision_mean', 'test_precision_sd', 
+        'method', 'test_precision_mean', 'test_precision_sd',
         'test_accuracy_mean', 'test_accuracy_sd','test_roc_auc_mean', 'test_roc_auc_sd',
         'fit_time_mean', 'fit_time_sd', 'score_time_mean', 'score_time_sd',
     ]
@@ -156,9 +156,9 @@ def run_classifiers(sk_func_dict, X, Y, scores=['accuracy', 'precision', 'roc_au
     size_clfs = len(sk_func_dict)
     for idx, method in enumerate(sk_func_dict):
         print(f"({idx+1}/{size_clfs}): {method} is running!", end="", file=stderr)
-        
+
         cv_metrics = cross_validate(sk_func_dict[method], X, Y, cv=kf, scoring=scores)
-        
+
         dict_metrics = {i:{'mean':np.mean(v), 'sd':np.std(v)} for i, v in cv_metrics.items()}
         dict_metrics["method"] = method
         df_metrics = pd.json_normalize(dict_metrics, sep="_")
@@ -218,7 +218,7 @@ def explore_data(df, name, DPI=226):
     # legend elements
     red_txt = Line2D([], [], marker=MarkerStyle("$123$"),
                 color=COLORS["NA"],linestyle='None', markersize=14)
-    black_txt = Line2D([], [], marker=MarkerStyle("$123$"), 
+    black_txt = Line2D([], [], marker=MarkerStyle("$123$"),
                 color="black", linestyle='None', markersize=14)
     f1, f2 = Patch(facecolor=COLORS["float"][0]), Patch(facecolor=COLORS["float"][1])
     i1, i2 = Patch(facecolor=COLORS["integer"][0]), Patch(facecolor=COLORS["integer"][1])
@@ -235,7 +235,7 @@ def explore_data(df, name, DPI=226):
         "columnspacing" :        -0.5,
         "handlelength" :        1.0,
         "handletextpad" :       0.5,
-        "framealpha" :          1, 
+        "framealpha" :          1,
         "edgecolor" : "black"
     }
 
@@ -249,29 +249,29 @@ def explore_data(df, name, DPI=226):
     #new plot
     fig, ax = plt.subplots(dpi=DPI)
     xmax = df.shape[0]
-        
+
     #populate the bar plot
     for key, serie in df.items():
-        
+
         unique = serie.value_counts(dropna=False).sort_values(ascending=False)
         cumul = 0
         type = __column_typer(serie)
- 
-        for idx, (modalite, count) in enumerate(unique.items()): 
-           
+
+        for idx, (modalite, count) in enumerate(unique.items()):
+
             if pd.isna(modalite):
                 c = COLORS["NA"]
             elif type in COLORS.keys():
                 c = COLORS[type][0] if idx%2 == 0 else COLORS[type][1]
             else:
                 print(f"err with col:{key} => {idx}, ({modalite}, {count}, {type(modalite)}")
-        
+
             ax.barh(y=str(key), width=count, left=cumul, color=c)
             cumul += count
 
         ax.text(xmax*1.05, key, unique.index.notna().sum(), **txt_args)
         ax.text(xmax*1.15, key, serie.isna().sum(), color=COLORS["NA"], **txt_args)
-    
+
     # adjust the plot
     ax.set_xlim((0, xmax*1.2))
     ax.set_title(f"Diversity of value per column in '{name}'")
@@ -279,19 +279,19 @@ def explore_data(df, name, DPI=226):
     ax.set_ylabel("Columns/variables")
 
     # set legend
-    ax.legend(  
+    ax.legend(
         handles=[f1, i1, s1, b1, n1, null, null,
                  f2, i2, s2, b2,n1, black_txt, red_txt],
         labels=['', '', '', '', '', '', '',
-                'continuous', 'discrete', 'categorical', 'binary','NA', 'unique values', 'NA values'], 
+                'continuous', 'discrete', 'categorical', 'binary','NA', 'unique values', 'NA values'],
         **lgd_args)
 
 
 def comparative_preprocessing(df, scores=["precision", "accuracy", "recall"], DPI=226):
     PREPRO = {
-        "" : 0, 
+        "" : 0,
         "MinMaxScale" : 1,
-        "StdScale" : 2, 
+        "StdScale" : 2,
         "StdScale + PCA" : 3,
         "Trim + StdScale": 4,
         "Trim + StdScale + PCA": 5
@@ -300,7 +300,7 @@ def comparative_preprocessing(df, scores=["precision", "accuracy", "recall"], DP
 
     fig, ax = plt.subplots(ncols=len(scores), figsize=(12, 4), dpi=DPI)
     for i, sc in enumerate(scores):
-        
+
         ax[i].set_xticks(
             list(range(len(LABELS))),
             labels=LABELS,
@@ -314,24 +314,24 @@ def comparative_preprocessing(df, scores=["precision", "accuracy", "recall"], DP
             dat = dat.sort_values(by="x")
             ax[i].plot(dat["x"], dat[sc], label=met)
 
-    hd, lb = ax[0].get_legend_handles_labels()    
+    hd, lb = ax[0].get_legend_handles_labels()
     fig.legend(
-        hd, lb, 
+        hd, lb,
         loc='right',
-        bbox_to_anchor=(1.08, 0.5), 
-        framealpha = 1, 
+        bbox_to_anchor=(1.08, 0.5),
+        framealpha = 1,
         edgecolor = "black"
     )
 
 
 def results_run_clfs(
-    run_df, run_name, top=10, height=5, DPI=226, 
+    run_df, run_name, top=10, height=5, DPI=226,
     scores=["test_accuracy", "test_roc_auc", "test_precision"]
 ):
 
     COLOR_CLFS = {
         "RF"    : colormaps["tab10"].colors[0],
-        "Bag"   : colormaps["tab10"].colors[1], 
+        "Bag"   : colormaps["tab10"].colors[1],
         "AdaB"  : colormaps["tab10"].colors[2],
         "NB"    : colormaps["tab10"].colors[3],
         "MLP"   : colormaps["tab10"].colors[4],
@@ -348,11 +348,11 @@ def results_run_clfs(
         xmin, xmax = run_df[mean][idx].tolist()[-1], run_df[mean][idx].tolist()[0]
         xmin, xmax = xmin*0.9, xmax*1.1
         vmax, vlast, vmin = run_df[mean].max(), run_df[mean][idx].min(), run_df[mean].min()
-        
+
         ax[i].set_xlim([xmin, xmax])
         ax[i].tick_params(axis='both', labelsize='x-small')
         ax[i].set_title(f"'{sc}' max:{vmax:.3f} last:{vlast:.3f} min:{vmin:.3f}", fontsize='small')
-      
+
         for r in idx[::-1]: # n methods loop
             serie = run_df.iloc[r]
             c = COLOR_CLFS[serie["method"].split("_")[0]]
@@ -362,7 +362,7 @@ def results_run_clfs(
     fig.tight_layout()
     fig.legend(
         [Patch(facecolor=COLOR_CLFS[key]) for key in COLOR_CLFS],[key for key in COLOR_CLFS],
-        frameon=False, 
+        frameon=False,
         bbox_to_anchor=(1.1, 0.75)
     )
 
